b7b934afbe86bb173a6f3749ff0c356a0d5c101b
Dansz <costinasdan35@yahoo.com>
2021-07-22 16:59:33 +0300

    Avg temp

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 
 #include <gtk-2.0/gtk/gtk.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
 #define BUFFERSIZE 34
+
+static double avg = 0.0f;
+static double reads  = 0.0f;
+
 void end_program (GtkWidget *wid, gpointer ptr)
     {
     gtk_main_quit ();
@@ -27,12 +32,12 @@ void dotString(char* str)
     }
 
 //reads the temperature into the string
-void readTemperature(char** string, int* len)
+void readTemperature(char** string, size_t* len)
 {
     FILE* tempFILE = fopen("/sys/class/thermal/thermal_zone0/temp","r");
     if(tempFILE == NULL)
     {
-         printf("COULD NOT OPEN TEMPERATURE FILE\n");
+            printf("COULD NOT OPEN TEMPERATURE FILE\n");
     }
         
     getline(string,len,tempFILE);
@@ -46,16 +51,17 @@ int update(unsigned char ** data)
         char* curTempString = (char*) data[1];
         char* minTempString = (char*) data[2];
         char* maxTempString = (char*) data[3];
-        int len = BUFFERSIZE;
+        size_t len = BUFFERSIZE;
         readTemperature(&curTempString,&len);
         sscanf(curTempString,"%lu",&temps[0]);
+        
         //check min temp
         if(temps[0] < temps[1])
         {
             strncpy(minTempString,curTempString,BUFFERSIZE);
             temps[1] = temps[0];
             dotString(minTempString);
-            gtk_label_set_text((GtkWidget*)data[5] , minTempString);
+            gtk_label_set_text((GtkLabel*)data[5] , minTempString);
         }
         //check max temp
         if(temps[0] > temps[2])
@@ -63,14 +69,22 @@ int update(unsigned char ** data)
             strncpy(maxTempString,curTempString,BUFFERSIZE);
             temps[2] = temps[0];
             dotString(maxTempString);
-            gtk_label_set_text((GtkWidget*)data[6] , maxTempString);
+            gtk_label_set_text((GtkLabel*)data[6] , maxTempString);
         }
 
-       
-
+  
+        //current
         dotString(curTempString);
-        gtk_label_set_text((GtkWidget*)data[4],curTempString);
+        gtk_label_set_text((GtkLabel*)data[4],curTempString);
 
+        //average 
+        double curTDouble = atof(curTempString);
+        avg *= reads;
+        reads += 1.0f;
+        avg += curTDouble;
+        avg /= reads;
+        snprintf(data[7], BUFFERSIZE,"%.3lf",avg);
+        gtk_label_set_text((GtkLabel*)data[8],data[7]);
         
 
 
@@ -85,7 +99,7 @@ int main (int argc, char *argv[])
     GtkWidget* win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     g_signal_connect (win, "delete_event", G_CALLBACK (end_program),NULL);
     gtk_window_set_default_size(win , 300 ,50 );
-    GtkWidget* table = gtk_table_new(3,5,0);
+    GtkWidget* table = gtk_table_new(4,6,0);
 
 
 
@@ -96,6 +110,8 @@ int main (int argc, char *argv[])
     gtk_table_attach_defaults(GTK_TABLE(table),mintemp,2,3,1,2);
     GtkWidget* maxtemp = gtk_label_new("0.0");
     gtk_table_attach_defaults(GTK_TABLE(table),maxtemp,3,4,1,2);
+    GtkWidget* avgtemp = gtk_label_new("0.0");
+    gtk_table_attach_defaults(GTK_TABLE(table),avgtemp,4,5,1,2);
     //First Row
     GtkWidget* curText = gtk_label_new(" Current ");
     gtk_table_attach_defaults(GTK_TABLE(table),curText,1,2,0,1);
@@ -103,6 +119,8 @@ int main (int argc, char *argv[])
     gtk_table_attach_defaults(GTK_TABLE(table),minText,2,3,0,1);
     GtkWidget* maxText = gtk_label_new("    Max   ");
     gtk_table_attach_defaults(GTK_TABLE(table),maxText,3,4,0,1);
+    GtkWidget* avgText = gtk_label_new("    AVG   ");
+    gtk_table_attach_defaults(GTK_TABLE(table),avgText,4,5,0,1);
     //
     //Temp text
     GtkWidget* tempText = gtk_label_new("Temp °C: ");
@@ -118,9 +136,9 @@ int main (int argc, char *argv[])
     char* curTempString = malloc(BUFFERSIZE * sizeof(char));
     char* minTempString = malloc(BUFFERSIZE* sizeof(char));
     char* maxTempString = malloc(BUFFERSIZE * sizeof(char));
-   
+    char* avgTempString = malloc(BUFFERSIZE* sizeof(char));
     
-    unsigned char** data[7] = {tempsInNumbers,curTempString,minTempString,maxTempString,curtemp,mintemp,maxtemp};
+    char** data[9] = {tempsInNumbers,curTempString,minTempString,maxTempString,curtemp,mintemp,maxtemp,avgTempString,avgtemp};
     
     g_timeout_add_seconds(1.0, update, data);
     gtk_widget_show_all (win);
@@ -132,5 +150,6 @@ int main (int argc, char *argv[])
     free(curTempString);
     free(minTempString);
     free(maxTempString);
+    free(avgTempString);
     return 0;
 }
